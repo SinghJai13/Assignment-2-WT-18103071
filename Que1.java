ac2e2303a1021ea7553ae0a6c22f195b81f1fdb5
SinghJai13 <46139190+SinghJai13@users.noreply.github.com>
2020-09-04 21:33:05 +0530

    Update Que1.java

--- Que1.java
+++ Que1.java
@@ -3,34 +3,34 @@ import java.util.Scanner;
 public class Que1{
 
 	public static  void longer(String str1,String str2){
-		int a=0;
-		int b=0;
+		int sum_of_str1=0;
+		int sum_of_str2=0;
 
 		for(int i=0;i<str1.length();i++)
 		{
-			a+=(int)(str1.charAt(i));
+			sum_of_str1+=(int)(str1.charAt(i));
 
 
 		}
 		for(int i=0;i<str2.length();i++)
 		{
-			b+=(int)(str2.charAt(i));
+			sum_of_str2+=(int)(str2.charAt(i));
 
 
 		}
 
-		if(a==b)
+		if(sum_of_str1==sum_of_str2)
 		{
-			System.out.println("Strings are lexicographically equal ");
+			System.out.println("Strings are lexicographically equal .");
 			return;
 		}
 
-		if(a>b)
+		if(sum_of_str1>sum_of_str2)
 		{
-			System.out.println("String 1 is lexicographically greater than String 2");
+			System.out.println("String 1 is lexicographically greater than String 2.");
 			return;
 		}
-		System.out.println("String 2 is lexicographically greater than String 1 ");
+		System.out.println("String 2 is lexicographically greater than String 1 .");
 		return ;
 
 	}
@@ -40,12 +40,12 @@ public class Que1{
 		
 		System.out.println("enter the string 1   :- ");
 
-		String str=sc.nextLine();
+		String str1=sc.nextLine();
 
 		System.out.println("enter the string 2 :- ");
 		
-		String sub=sc.nextLine();
-		longer(str,sub);
+		String str2=sc.nextLine();
+		longer(str1,str2);
 
 }
 }
